e5d4df49fcb7eccf70d6aff20210dfd9e86a111f
SurfingInWavelets <TT-Tor@gmx.net>
2017-02-02 20:50:54 +0100

    pointEvaluation outsourced (->refinement.js)
    improved Ftemplate3

--- js/sites/Ftemplate3.js
+++ js/sites/Ftemplate3.js
@@ -7,9 +7,13 @@
  */
 
 function setHtmlFunctions() {
+	//load given Data from page before. (Ftemplate2)
+	//var id = getQueryVariable("id");
+	//var table = getQueryVariable("table");
 	
-	var id = getQueryVariable("id");
-	var table = getQueryVariable("table");
+	//to test:
+	var id = 6;
+	var table = "BiMRA";
 	
 	/////////////////////////////////////////
 	//  1. Abschnitt/////////////////////////
@@ -22,7 +26,7 @@ function setHtmlFunctions() {
 	//  2.Abschnitt ///////////////////////////
 	///////////////////////////////////////////
 	
-	if(table=="OMRA"){
+	if(table=="OMRA"||table=="BiMRA"){
 		var mask = JSON.parse(db.exec("SELECT mask FROM " + table + " WHERE id = " + id)[0].values[0][0]);
 		var a_start = db.exec("SELECT a_start FROM " + table + " WHERE id = " + id)[0].values[0][0];
 		console.log('mask',  mask, 'astart', a_start);
@@ -31,26 +35,99 @@ function setHtmlFunctions() {
 	}
 	
 	///////////////////////////////////////////
-	//   3.Abschnitt //////////////////////////
+	//   3.Abschnitt (Information) ////////////
 	///////////////////////////////////////////
 	var scf = db.exec("SELECT * FROM " + table + " WHERE id = " + id)[0];
 	console.log ('scf', scf);
 	
-	var str = generateInfoString(scf);
+	var str = generateInfoString(table,scf);
 	$(info).val(str);
+	
+	///////////////////////////////////////////
+	// 4. Abschnitt (duale scf) ///////////////
+	///////////////////////////////////////////
+	
+	if(table=="BiMRA"){
+		var dualIds = JSON.parse(db.exec("SELECT ID_of_dual_function FROM " + table + " WHERE id = " + id)[0].values[0][0]);
+		
+		
+		//adding all possible dual functions to select menue
+		if(dualIds.constructor === Array){
+			for(var j=0; j< dualIds.length; j++){			
+				$('#select-dual-scfs').append($('<option>', {
+	   				value: db.exec("SELECT ID FROM " + table + " WHERE id = " + dualIds[j])[0].values[0][0],
+	    			text: db.exec("SELECT name FROM " + table + " WHERE id = " + dualIds[j])[0].values[0][0]
+				}));
+			}
+		}
+		else{$('#select-dual-scfs').append($('<option>', {
+	   				value: db.exec("SELECT ID FROM " + table + " WHERE id = " + dualIds)[0].values[0][0],
+	    			text: db.exec("SELECT name FROM " + table + " WHERE id = " + dualIds)[0].values[0][0]
+			}));
+		}
+		
+	}
+	else{
+		$(dscf).hide();
+	}
+	
+	
+	$('#select-dual-scfs').change(function(){
+		if(table=="BiMRA"){
+		
+			var dualID = $('#select-dual-scfs').val();
+			var dualMask = JSON.parse(db.exec("SELECT mask FROM " + table + " WHERE id = " + dualID)[0].values[0][0]);
+			console.log('dualMask',dualMask);
+			var a_tilde_start=db.exec("SELECT a_start FROM " + table + " WHERE id = " + dualID)[0].values[0][0];
+			valuesDer=iterativePointEvaluation2(dualMask, a_tilde_start, 8, 0);  
+			plotInstance2.draw();
+		}
+	});
+	
+	///////////////////////////////////////////
+	//  5.Abschnitt (Wavelet-Plot) ////////////
+	///////////////////////////////////////////
+	$('#select-dual-scfs').change(function(){
+		if(table=="OMRA"||table=="BiMRA"){
+				var mask = JSON.parse(db.exec("SELECT mask FROM " + table + " WHERE id = " + id)[0].values[0][0]);
+				var a_start = db.exec("SELECT a_start FROM " + table + " WHERE id = " + id)[0].values[0][0];
+				var dualMask;
+				var a_tilde_start;
+				if(table=="OMRA"){
+					dualMask = mask;
+					a_tilde_start=a_start;
+				}
+				if(table=="BiMRA"){
+					var dualID = $('#select-dual-scfs').val();
+					console.log('dualID',dualID);
+					console.log(db.exec("SELECT mask FROM " + table + " WHERE id = " + dualID)[0].values[0]);
+					dualMask = JSON.parse(db.exec("SELECT mask FROM " + table + " WHERE id = " + dualID)[0].values[0][0]);
+					console.log('dualMask',dualMask);
+					a_tilde_start=db.exec("SELECT a_start FROM " + table + " WHERE id = " + dualID)[0].values[0][0];
+				}
+				
+				console.log('mask',  mask, 'astart', a_start);
+				valuesWav=waveletPointEvaluation(mask, a_start, dualMask, a_tilde_start, 8); 
+				plotInstance3.draw();
+			}
+		});
 	
 }
 
 // generates an string containing all information given in one database line
 // input: scf - object containing line and columns of the choosen funktion in Database
-function generateInfoString(scf) {
-		var info = "";
+function generateInfoString(table,scf) {
+		var info = "table: " + table +"\n";
 		for(var column = 0; column < scf.columns.length; column++){
 			//info += "<p> ";
 			info += scf.columns[column];
 			info += ": ";
-			info += scf.values[0][column] + "\r";
+			info += scf.values[0][column] + "\n";
 		}
 		console.log('info', info);
 		return info;	
+}
+
+function plotWavelet(){
+	
 }
---
+++ outdated/pointEvaluation2.js
@@ -0,0 +1,61 @@
+/** This file contains functions for point evaluations of wavelets
+ *  with given coefficients of the (dual) scaling function
+ *
+ *  Dependencies: pointEvaluation.js, mask.js
+ *
+ *	(last modification: 18.5.16 Andreas)
+ */
+
+/** Evaluate the points of a wavelet by its representation as a sum of
+ *  weighted translations of the scaling function with weights from the dual
+ *  scaling function.
+ * 
+ *  @param{Array}	a				refinement coefficients of the generator.
+ *  @param{int}		a_start			the start index of the refinement coefficients.
+ *  @param{Array} 	a_tilde the 	refinement coefficients of the dual generator.
+ * 	@param{int}		a_tilde_start	the index of the first non-vanishing 
+ * 									refinement coefficient.
+ * 	@param{int} 	j 				(1/2)^(-j) 	the step size of the x-lattice.
+ *
+ * 	@return{Array} 	values  the array of x-lattice-points and the corresponding
+ * 							wavelet-values at these points in the form [x,y].
+ */
+// ist es notwendig a und a_tilde zu ubergeben, statt nur a ?
+function waveletPointEvaluation(a, a_start, a_tilde, a_tilde_start, j){
+	var N = a.length - 1; //support of phi
+	var N_tilde = a_tilde.length - 1;  //support of phi-tilde
+	var sf_values = iterativePointEvaluation2(a, a_start, j - 1, 0);
+	var b_array = refCoeffstoWaveletCoeffs(a_tilde, a_tilde_start);
+	var b = b_array[0]; //achtung evtl. b zerstoert.
+	var b_start = b_array[1];
+	
+	//create the x-lattice with dyadic resolution j
+	var pow2_j = Math.pow(2, j);
+	var pow2_j_minus1 = Math.pow(2, j - 1);
+	var pow2_minusj_plus1 = 1/pow2_j_minus1;
+	
+	var wavelet_support = (N_tilde + N)/2;
+	var values = createArray(wavelet_support * pow2_j + 1, 2);
+	
+	for(var i = 0; i < values.length; i++){
+		values[i][0] = i/pow2_j + a_start;	
+	}
+	
+	var result = 0;		
+	for(var m = 0; m < values.length; m++){
+		result = 0;
+		
+		var k1 = Math.max(0, Math.ceil(pow2_minusj_plus1 * m) - N);
+		var k2 = Math.min(N_tilde, Math.floor(pow2_minusj_plus1 * m));
+		
+		
+		for(var k = k1; k < k2 + 1; k++)
+		{	
+			result +=  b[k]*sf_values[m - pow2_j_minus1*k][1];
+		}
+		values[m][1] = result;
+	}
+	return values;
+}
+
+
